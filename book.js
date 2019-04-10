39b2e7172ba076bb373faf0c221e80f5fc620b05
Simon Tharby <jinjamail@gmail.com>
2019-04-10 16:59:35 +0100

    read properties from array of Book objects and display in html table

--- book.js
+++ book.js
@@ -5,17 +5,38 @@ function Book(title, author, pages, haveRead) {
   this.haveRead = haveRead
 }
 
-// It's better to write functions in prototype, so that a single instance of
+// It's better to write functions in prototype, so that a single instance of the
 // function is shared between all child objects.
 Book.prototype.info = function() {
   return `${this.title}, by ${this.author}, ${this.pages} pages, ${this.haveRead == true ? 'have read' : 'not read yet'}`;
 }
 
-const myBook = new Book('The Thing', 'Some Dude', 567, false);
+function render(array) {
+  for (let i = 0; i < myLibrary.length; i++) {
+    const row = document.createElement('tr');
+    for (let j = 0; j < 4; j++) {
+      const data = document.createElement('td');
+      data.innerHTML = myLibrary[i][Object.keys(myLibrary[i])[j]];
+      if (j > 1) data.style.textAlign = "center";
+      row.appendChild(data);
+    }
+    table_books.appendChild(row);
+  }
+}
 
-// toString() is a method in Object.prototype
-console.log(myBook.toString());
+let myLibrary = [];
 
-console.log(myBook);
+myLibrary.push(new Book('The Thing', 'Some Dude', 567, false));
+myLibrary.push(new Book('Ubiq', 'Philip K. Dick', 223, true));
 
-console.log(myBook.info());
+let table_books = document.getElementById("books_table");
+
+render(myLibrary);
+
+
+
+
+// Unused, for now:
+
+//data.classList.add('data');
+//data.id = i.toString();

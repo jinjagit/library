09e516d48730bef5ee0f5275e22dce82e0ab3e21
Simon Tharby <jinjamail@gmail.com>
2019-04-12 14:57:19 +0100

    add check for local storage

--- main.js
+++ main.js
@@ -70,6 +70,8 @@ function addBook() {
     document.form.read.checked? haveRead = true : haveRead = false;
     myLibrary.push(new Book(title, author, pages, haveRead));
   }
+
+  render(myLibrary);
 }
 
 function removeBook(index) {
@@ -84,6 +86,38 @@ function toggleRead(index) {
     myLibrary[index]['haveRead'] = true;
   }
   render(myLibrary);
+}
+
+function storageAvailable(type) { // from: https://developer.mozilla.org
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
+if (storageAvailable('localStorage')) {
+  console.log("Yippee! We can use localStorage awesomeness");
+}
+else {
+  console.log("Too bad, no localStorage for us");
 }
 
 let formBook = document.querySelector('.form-book');
@@ -101,7 +135,6 @@ formShow.addEventListener("click", function() {
 
 formSubmit.addEventListener("click", function() {
   addBook();
-  render(myLibrary);
 });
 
 let myLibrary = [];
